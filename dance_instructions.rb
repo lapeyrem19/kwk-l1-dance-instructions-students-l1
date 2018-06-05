1539974af5126662156dd9223fc23803df38d11b
lapeyrem19 <lapeyrem19@ashevilleschool.org>
2018-06-05 15:10:47 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,23 @@
+def assume_stance
+puts "Plant legs far apart, bend knees slightly and keep posture loose."
+end 
 
-# Code your methods below
+
+def base_footwork
+  puts "Lift right foot
+  puts "Return right foot to the ground"
+puts "Finishing with a small skip-step backward
+end 
+
+
+def skip_step_left
+  puts "Lower left foot to the ground"
+  puts "Bounce left foot back up slightly, kicking it a few inches back"
+  puts "Left, Right, Left and Left"
+end 
+
+
+def
+
+
+
