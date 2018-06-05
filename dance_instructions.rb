946fc7225129d7af1edcd1ac03fd90931565460d
lapeyrem19 <lapeyrem19@ashevilleschool.org>
2018-06-05 15:16:56 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -21,6 +21,21 @@ def skip_step_right
   puts "Lower right foot to the ground"
   puts "Bounce right foot back up slightly, kicking it a few inches back"
   puts "Right, Left, Right and Right"
+end
+
+def bob_the_reins
+  puts "olding your arms out in front of you, straight and at chest level"
+  puts "Cross your right wrist over your left and hold them together"
+  puts "Lift your arms up and down in a loose bouncing movement, in time with the beat of the song. This movement is repeated eight times"
 end 
 
+def lasso
+  puts "Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left."
+  puts "Lift right arm up and point right elbow diagonally to the right."
+  puts "Raise right forearm so that it points straight up, and whip it in small circles to the beat of the song, as if you were a cowboy holding a rope lasso. This movement is repeated eight times as well."
+end 
 
+def bust_a_move
+  puts "Be cool"
+  puts "Be snazzy"
+end 
