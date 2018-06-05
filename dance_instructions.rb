a09d1ae0e228336442eb5cddf27b6790a805b8a5
lapeyrem19 <lapeyrem19@ashevilleschool.org>
2018-06-05 15:20:24 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -27,15 +27,23 @@ def bob_the_reins
   puts "olding your arms out in front of you, straight and at chest level"
   puts "Cross your right wrist over your left and hold them together"
   puts "Lift your arms up and down in a loose bouncing movement, in time with the beat of the song. This movement is repeated eight times"
-end 
+end
 
 def lasso
   puts "Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left."
   puts "Lift right arm up and point right elbow diagonally to the right."
   puts "Raise right forearm so that it points straight up, and whip it in small circles to the beat of the song, as if you were a cowboy holding a rope lasso. This movement is repeated eight times as well."
-end 
+end
 
 def bust_a_move
   puts "Be cool"
   puts "Be snazzy"
-end 
+end
+
+assume_stance
+bust_a_move
+base_footwork
+bob_the_reins
+skip_step_left
+skip_step_right
+puts ""
